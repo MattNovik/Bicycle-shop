da2ffecb0888900dc8b3db418f41c5386e91c891
Godicks <matvej.novik@gmail.com>
2021-09-28 14:46:20 +0300

    finish with pics

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,10 @@
-/* 'use strict';
+'use strict';
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
 
 pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
+headerToggle.addEventListener('click', () => {
   if (pageHeader.classList.contains('page-header--closed')) {
     pageHeader.classList.remove('page-header--closed');
     pageHeader.classList.add('page-header--opened');
@@ -13,4 +13,3 @@ headerToggle.addEventListener('click', function () {
     pageHeader.classList.remove('page-header--opened');
   }
 });
- */
